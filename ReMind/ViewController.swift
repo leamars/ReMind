8315596b3ba894574ddf25a8278553d39920f4fe
Lea Marolt Sonnenschein <leyamars@gmail.com>
2021-05-03 21:33:54 +0100

    Adding new song + speed up, slow down capabilities as well as a speed label and the ability to recognize words to trigger certain functinos.

--- ReMind/ViewController.swift
+++ ReMind/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import AudioKit
 import AudioKitUI
+import Speech
+import AVFoundation
 
 enum PlaybackState {
     case moving
@@ -16,7 +18,15 @@ enum PlaybackState {
 
 class ViewController: UIViewController {
     
+    // Speech Recognition
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    var mostRecentlyProcessedSegmentDuration: TimeInterval = 0
+    
     var player: AKPlayer!
+    var timePitch: AKTimePitch!
     var completionHandler: AKCallback?
     var splits: [Split] = []
     var selectedSplit: Split?
@@ -28,6 +38,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var timeSlider: UISlider!
     @IBOutlet weak var startLabel: UILabel!
     @IBOutlet weak var endLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
     
     @IBOutlet weak var leadingPlayheadConstraint: NSLayoutConstraint!
     @IBOutlet weak var playHead: UIView!
@@ -68,13 +79,15 @@ class ViewController: UIViewController {
         setupPlayer()
         setupGestureRecognizers()
         setupPlaybackTimer()
+        setupSpeechRecognition()
         
         timeSlider.value = 0
         collectionView.automaticallyAdjustsScrollIndicatorInsets = false
         
-        
         previousHapticX = leadingPlayheadConstraint.constant
         originalPlayheadLeadingConstant = leadingPlayheadConstraint.constant
+        
+        collectionView.isScrollEnabled = false
     }
     
     private func setupGestureRecognizers() {
@@ -93,10 +106,25 @@ class ViewController: UIViewController {
         collectionView.addGestureRecognizer(doubleTap)
         
         tapGestureRecognizer.require(toFail: doubleTap)
+        
+        let pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(recognizer:)))
+        collectionView.addGestureRecognizer(pinchRecognizer)
+        
+        let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeUp(recognizer:)))
+        swipeUpGesture.numberOfTouchesRequired = 1
+        swipeUpGesture.direction = .up
+        swipeUpGesture.delegate = self
+        collectionView.addGestureRecognizer(swipeUpGesture)
+        
+        let swipeDownGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeDown(recognizer:)))
+        swipeDownGesture.numberOfTouchesRequired = 1
+        swipeDownGesture.direction = .down
+        swipeDownGesture.delegate = self
+        collectionView.addGestureRecognizer(swipeDownGesture)
     }
     
     private func setupPlayer() {
-        let path = Bundle.main.path(forResource: "zemlja", ofType: "mp3")!
+        let path = Bundle.main.path(forResource: "HotelSong", ofType: "mp3")!
         let url = URL(fileURLWithPath: path)
         
         player = AKPlayer(url: url)
@@ -109,7 +137,9 @@ class ViewController: UIViewController {
             self.player.play(from: split.startTime, to: split.endTime)
         }
         
-        AKManager.output = player
+        timePitch = AKTimePitch(player)
+        timePitch.rate = 1.0
+        AKManager.output = timePitch
         try! AKManager.start()
         
         let newDurationSeconds = Float(player.duration)
@@ -147,6 +177,97 @@ class ViewController: UIViewController {
                 self.updateTimeStrings()
             }
         }
+    }
+    
+    private func setupSpeechRecognition() {
+        try! AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord,
+                                                         options: [AVAudioSession.CategoryOptions.defaultToSpeaker, .allowBluetoothA2DP, .allowAirPlay])
+        
+        SFSpeechRecognizer.requestAuthorization {
+          [unowned self] (authStatus) in
+          switch authStatus {
+          case .authorized:
+            do {
+              try self.startRecording()
+            } catch let error {
+              print("There was a problem starting recording: \(error.localizedDescription)")
+            }
+          case .denied:
+            print("Speech recognition authorization denied")
+          case .restricted:
+            print("Not available on this device")
+          case .notDetermined:
+            print("Not determined")
+          @unknown default: break
+          }
+        }
+    }
+    
+    private func startRecording() throws {
+      mostRecentlyProcessedSegmentDuration = 0
+
+      // 1
+      let node = audioEngine.inputNode
+      let recordingFormat = node.outputFormat(forBus: 0)
+
+      // 2
+      node.installTap(onBus: 0, bufferSize: 1024,
+                      format: recordingFormat) { [unowned self]
+                        (buffer, _) in
+                        self.request.append(buffer)
+      }
+
+      // 3
+      audioEngine.prepare()
+      try audioEngine.start()
+      recognitionTask = speechRecognizer?.recognitionTask(with: request) {
+        [unowned self]
+        (result, _) in
+        if let transcription = result?.bestTranscription,
+           let lastSegment = transcription.segments.last
+           //lastSegment.duration > mostRecentlyProcessedSegmentDuration
+            //print(transcription.formattedString)
+           {
+            
+            mostRecentlyProcessedSegmentDuration = lastSegment.duration
+            
+            switch lastSegment.substring {
+            case "cut":
+                splitTrack()
+            case "add":
+                // Puts tracks together again
+                combineLoopingSplitWithNext()
+            case "up":
+                changeTime(by: 0.1)
+            case "down":
+                changeTime(by: -0.1)
+            case "loop":
+                if let selectedIndex = indexPathAtPlayheadPoint {
+                    selectedSplit = splits[selectedIndex.row]
+                    isLooping = true
+
+                    playMusic(shouldSwitch: false, loop: true, from: selectedSplit!.startTime, to: selectedSplit!.endTime)
+                }
+            case "play":
+                playMusic(shouldSwitch: true)
+            case "stop":
+                // TODO: This should probably start it form the beginning
+                playMusic(shouldSwitch: true)
+            case "pause":
+                playMusic(shouldSwitch: true)
+            case "start":
+                // Plays from the start
+                break
+            default: break
+            }
+        }
+      }
+    }
+    
+    private func stopRecording() {
+      audioEngine.stop()
+      request.endAudio()
+      recognitionTask?.cancel()
     }
     
     // MARK: - Gesture recognizers
@@ -212,9 +333,73 @@ class ViewController: UIViewController {
         }
     }
     
+    @objc func handlePinch(recognizer: UIPinchGestureRecognizer) {
+        
+        switch recognizer.state {
+        case .ended:
+            // If player is looping, combine the looping split with the next one
+            combineLoopingSplitWithNext()
+        case .began, .possible, .cancelled, .failed, .changed: break
+        @unknown default: break
+        }
+    }
+    
+    private func combineLoopingSplitWithNext() {
+        if let selectedSplit = selectedSplit, isLooping,
+           let indexOfSelectedSplit = splits.firstIndex(where: { $0.startTime == selectedSplit.startTime }),
+           splits.count > indexOfSelectedSplit + 1 {
+            
+            let nextSplit = splits[indexOfSelectedSplit+1]
+            let combinedSplit = Split(startTime: selectedSplit.startTime, endTime: nextSplit.endTime)
+            
+            splits.remove(at: indexOfSelectedSplit)
+            splits.remove(at: indexOfSelectedSplit)
+            
+            splits.insert(combinedSplit, at: indexOfSelectedSplit)
+            
+            self.selectedSplit = combinedSplit
+            playMusic(shouldSwitch: false, loop: true, from: combinedSplit.startTime, to: combinedSplit.endTime)
+            
+            collectionView.reloadData()
+        }
+    }
+    
+    @objc func handleSwipeUp(recognizer: UISwipeGestureRecognizer) {
+        handleSwipe(recognizer: recognizer)
+    }
+    
+    @objc func handleSwipeDown(recognizer: UISwipeGestureRecognizer) {
+        handleSwipe(recognizer: recognizer)
+    }
+    
+    private func handleSwipe(recognizer: UISwipeGestureRecognizer) {
+        switch recognizer.state {
+        case .ended:
+            switch recognizer.direction {
+            case .down:
+                changeTime(by: -0.1)
+            case .up:
+                changeTime(by: 0.1)
+            case .left, .right: break
+            default: break
+            }
+        case .began, .possible, .cancelled, .failed, .changed: break
+        @unknown default: break
+        }
+    }
+    
+    // MARK: - IBActions
     @IBAction func playPauseTap(_ sender: UITapGestureRecognizer) {
         playMusic(shouldSwitch: true)
     }
+    @IBAction func turnRateUp(_ sender: Any) {
+        changeTime(by: 0.1)
+    }
+    
+    @IBAction func turnRateDown(_ sender: Any) {
+        changeTime(by: -0.1)
+    }
+    
     
     // Only gets triggered when user interacts with this
     @IBAction func timerSliderChanged(_ sender: UISlider) {
@@ -308,6 +493,13 @@ class ViewController: UIViewController {
 
             playMusic(shouldSwitch: false, loop: true, from: split.startTime, to: split.endTime)
         }
+    }
+    
+    private func changeTime(by delta: Double) {
+        timePitch.rate = timePitch.rate + delta
+        
+        let numString = String(format: "%.1f", timePitch.rate)
+        speedLabel.text = "\(numString)x"
     }
 }
 
